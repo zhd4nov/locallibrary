f410d3b6195db45f95fe91349e8df8f4e2f9e88c
zhd4nov <prctmlm@gmail.com>
2020-05-15 16:54:24 +0300

    add controller templates

---
+++ controllers/authorController.js
@@ -0,0 +1,41 @@
+const Author = require('../models/author');
+
+// Show all autors
+exports.author_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author list');
+};
+
+// Show author detail
+exports.author_detail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+};
+
+// Show creation form - get
+exports.author_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author create GET');
+};
+
+// Create Author - post
+exports.author_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author create POST');
+};
+
+// Show removing form - get
+exports.author_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete GET');
+};
+
+// Remove author - post
+exports.author_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete POST');
+};
+
+// Show update form
+exports.author_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author update GET');
+};
+
+// Update author
+exports.author_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author update POST');
+};
