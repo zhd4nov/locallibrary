ad6de4166d264509799bf0e5a16e38d71a2ae11e
zhd4nov <prctmlm@gmail.com>
2020-05-15 18:52:51 +0300

    calculation items count

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,29 @@
 const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
 
+const async = require('async');
 exports.index = (req, res) => {
-  res.send('NOT IMPLEMENTED: Site Home Page');
+  async.parallel({
+    book_count: (callback) => {
+      Book.count({}, callback);
+    },
+    book_instance_count: (callback) => {
+      BookInstance.count({}, callback);
+    },
+    book_instance_available_count: (callback) => {
+      BookInstance.count({ status: 'Available' }, callback);
+    },
+    author_count: (callback) => {
+      Author.count({}, callback);
+    },
+    genre_count: (callback) => {
+      Genre.count({}, callback);
+    },
+  }, (err, results) => {
+    res.render('index', { title: 'Local Library Home', error: err, data: results });
+  });
 };
 
 // Display list of all books.
