dbcf424403e853b36062e473724ba96ead09be41
zhd4nov <prctmlm@gmail.com>
2020-05-16 14:55:27 +0300

    form book detail page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -38,8 +38,34 @@ exports.book_list = (req, res) => {
 };
 
 // Display detail page for a specific book.
-exports.book_detail = (req, res) => {
-  res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+exports.book_detail = (req, res, next) => {
+  async.parallel({
+    book: (callback) => {
+      Book.findById(req.params.id)
+        .populate('author')
+        .populate('genre')
+        .exec(callback);
+    },
+    book_instance: (callback) => {
+      BookInstance.find({ 'book': req.params.id }).exec(callback);
+    },
+  }, (err, results) => {
+    if (err) { return next(err); }
+    if (results.book == null) { // No results.
+      var err = new Error('Book not found');
+      err.status = 404;
+      return next(err);
+    }
+    // Success:
+    res.render(
+      'book_detail',
+      {
+        title: results.book.title,
+        book: results.book,
+        book_instances: results.book_instance
+      }
+    );
+  });
 };
 
 // Display book create form on GET.
