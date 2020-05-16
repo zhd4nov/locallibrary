2ac9eb0eb0d09b924739868823c57bd60749833b
zhd4nov <prctmlm@gmail.com>
2020-05-16 12:14:29 +0300

    format due date

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,12 @@ BookInstanceSchema
   .virtual('url')
   .get(function () {
     return `/catalog/bookinstance/${this._id}`;
+  });
+
+BookInstanceSchema
+  .virtual('due_back_formatted')
+  .get(function () {
+    return moment(this.due_back).format('MMMM Do, YYYY');
   });
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
