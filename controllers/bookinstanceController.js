dcf9bf3483a12ed40d87198021d16854ce75e863
zhd4nov <prctmlm@gmail.com>
2020-05-16 12:02:42 +0300

    get book instance list

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -2,7 +2,13 @@ const BookInstance = require('../models/bookinstance');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance list');
+  BookInstance
+    .find()
+    .populate('book')
+    .exec((err, bookinstance_list) => {
+      if (err) return next(err);
+      res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list });
+    });
 };
 
 // Display detail page for a specific BookInstance.
