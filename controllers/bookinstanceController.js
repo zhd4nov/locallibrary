2e3b3ce42440f797d57ee68579a95317e9910f88
zhd4nov <prctmlm@gmail.com>
2020-05-16 15:41:54 +0300

    get book instace details

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -12,8 +12,23 @@ exports.bookinstance_list = (req, res) => {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = (req, res, next) => {
+  BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec((err, bookinstance) => {
+      if (err) return next(err);
+
+      if (bookinstance === null) {
+        let err = new Error('Book copy not found')
+        err.status = 404;
+        return next(err);
+      }
+      console.log(bookinstance);
+      res.render(
+        'bookinstance_detail',
+        { title: `Copy: ${bookinstance.book.title}`, bookinstance },
+      );
+    });
 };
 
 // Display BookInstance create form on GET.
