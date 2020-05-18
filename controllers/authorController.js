c8a3464b27cafacae58b1e9b5955e81a9bb190b2
zhd4nov <prctmlm@gmail.com>
2020-05-18 18:36:37 +0300

    feature(author) user can create new author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,8 @@
 const Author = require('../models/author');
 const Book = require('../models/book');
 const async = require('async');
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 // Show all autors
 exports.author_list = (req, res, next) => {
@@ -46,13 +48,61 @@ exports.author_detail = (req, res, next) => {
 
 // Show creation form - get
 exports.author_create_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author create GET');
+  res.render('author_form', { title: 'Create Author' });
 };
 
 // Create Author - post
-exports.author_create_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = (req, res) => [
+  // Validation
+  body('first_name').isLength({ min: 1 }).trim()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+  body('family_name').isLength({ min: 1 }).trim()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+  body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601(),
+
+  // Sanitize
+  sanitizeBody('first_name').escape(),
+  sanitizeBody('family_name').escape(),
+  sanitizeBody('date_of_birth').toDate(),
+  sanitizeBody('date_of_death').toDate(),
+
+  // Process
+  (req, res, next) => {
+    // Extract errors
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render(
+        'author_form',
+        {
+          title: 'Create Author',
+          author: req.body,
+          errors: errors.array(),
+        },
+      );
+
+      return;
+    }
+
+    const author = new Author(
+      {
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      }
+    );
+
+    author.save((err) => {
+      if (err) return next(err);
+
+      res.redirect(author.url);
+    });
+  }
+];
 
 // Show removing form - get
 exports.author_delete_get = (req, res) => {
