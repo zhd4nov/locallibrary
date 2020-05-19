f795b331f248a96191e3415feeace9e7f766756d
zhd4nov <prctmlm@gmail.com>
2020-05-19 16:04:55 +0300

    add hendler for bookinstance deletion

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -92,13 +92,21 @@ exports.bookinstance_create_post = [
 ];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = (req, res, next) => {
+  BookInstance.findById(req.params.id).exec((err, instance) => {
+    if (err) return next(err);
+
+    res.render('bookinstance_delete', { title: 'Delete Copy', instance });
+  });
 };
 
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = (req, res, next) => {
+  BookInstance.findByIdAndRemove(req.body.instanceid, (err) => {
+    if (err) return next(err);
+
+    res.redirect('/catalog/bookinstances');
+  })
 };
 
 // Display BookInstance update form on GET.
